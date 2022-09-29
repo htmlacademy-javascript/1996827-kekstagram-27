139f2d95460530aeb4a83086d09d479108659d4f
kos <ustyugovki@bk.ru>
2022-09-29 10:56:17 +0300

    tun js

--- js/main.js
+++ js/main.js
@@ -7,9 +7,13 @@
 // getSum (1, 4);
 // npm run lint
 // npm run lint -- --fix
+/**
+ * @param {number} a
+ * @param {number} b
+ */
 function getSum (a, b) {
   const description = 'сумма двух чисел равна';
-  const sum = a + b;
+  const sum = a + String(b);
   return `${description}${sum}`;
 }
 getSum (1, 4);
