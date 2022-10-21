ca4d7a9aa896997b6d323791692b212ae0122869
kos <ustyugovki@bk.ru>
2022-10-22 00:07:49 +0300

    Сделал начало module7-task1. КЛАССЫ

---
+++ js/components/component.js
@@ -0,0 +1,65 @@
+/**
+ * Экранирует html-символы
+ * @param {string} value
+ */
+ export const escapeHtml = (value) => {
+  const pattern = /[<>&"']/g;
+  const replacement = (match) => `&#${match.codePointAt(0)};`;
+
+  return value.replace(pattern, replacement);
+};
+
+/**
+ * Тег шаблонной html-строки
+ * @param {TemplateStringsArray} strings
+ * @param {...*} values
+ * @return {string}
+ */
+export const html = (strings, ...values) => strings.reduce((before, after, index) => {
+  const value = values[index - 1];
+
+  return before + escapeHtml(String(value)) + after;
+});
+
+/**
+ * Базовый компонент
+ */
+export default class Component extends HTMLElement {
+  /**
+   * @param {Object} [content]
+   */
+  constructor(content) {
+    super();
+
+    this.insertAdjacentHTML('beforeend', this.createHtml(content));
+  }
+
+  /**
+   * Создаст дополнительную html-разметку
+   * @param {Object} [content]
+   */
+  createHtml(content) {
+    return content ? html`
+      <pre>${JSON.stringify(content, null, 2)}</pre>
+    ` : '';
+  }
+
+  /**
+   * Переключит видимость компонента
+   * @param {boolean} flag
+   */
+  display(flag) {
+    this.classList.toggle('hidden', !flag);
+  }
+
+  /**
+   * Имя компонента в html
+   */
+  static get localName() {
+    return this.name.replace(/(?!^)[A-Z]/g, '-$&').toLowerCase();
+  }
+
+  static toString() {
+    return this.localName;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+import ImageGallery from './components/image-gallery.js';
 import generateImageStates from './images-generator.js';
 
-generateImageStates();
+
+const images = generateImageStates();
+
+/**
+ * @type {ImageGallery}
+ */
+const gallery = document.querySelector(String(ImageGallery));
+
+
+gallery.setContent(images);
+
