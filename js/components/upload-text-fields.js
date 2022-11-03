cd51d58ce2e341e434643fc497963a42aaf1ddca
kos <ustyugovki@bk.ru>
2022-11-03 06:27:49 +0300

    Сделал валидацию хештегов и коммита

--- js/components/upload-text-fields.js
+++ js/components/upload-text-fields.js
@@ -15,22 +15,29 @@ const list = (value) => value.split(' ').filter(Boolean);
 
 addValidator('item-pattern', (value, pattern, flag) => {
   const items = list(value);
-  // const [pattern, flag] = pattern.split(',');
   const regexp = RegExp(pattern, flag);
-  // debugger;
-  // console.log(items);
 
   return items.every((item) => regexp.test(item));
+});
+
+addValidator('item-duplicate', (value) => {
+  const items = list(value);
+  return new Set(items).size === items.length;
+});
+
+addValidator('item-amount', (value) => {
+  const items = list(value);
+  return items.length <= 5;
+});
+
+addValidator('item-length', (value) => {
+  const items = list(value);
+  return items.join('').length <= 19;
 });
 
 // TODO: один и тот же хэш-тег не может быть использован дважды;
 // TODO: максимальная длина одного хэш-тега 20 символов, включая решётку;
 // TODO: нельзя указать больше пяти хэш-тегов;
-
-// TODO: кнопка масштаба;
-
-// TODO: слайдер эффектов
-
 
 export default class UploadTextFields extends Component {
   constructor() {
@@ -53,13 +60,22 @@ export default class UploadTextFields extends Component {
           data-pristine-item-pattern="^#[a-zа-яё0-9]+$,i"
           data-pristine-item-pattern-message="С начала #, потом буквы / цифры"
 
+          data-pristine-item-duplicate="^#[a-zа-яё0-9]+$,i"
+          data-pristine-item-duplicate-message="Тэг повторяется"
+
+          data-pristine-item-amount="^#[a-zа-яё0-9]+$,i"
+          data-pristine-item-amount-message="Не больше 5 тэгов"
+
+          data-pristine-item-length="^#[a-zа-яё0-9]+$,i"
+          data-pristine-item-length-message="Не больше 20 символов"
+
           placeholder="#ХэшТег">
       </div>
       <div class="text__field">
         <textarea
           class="text__description"
           name="description"
-          data-pristine-maxlength ="5"
+          data-pristine-maxlength ="140"
           data-pristine-maxlength-message ="Не более \${1} символов"
           placeholder="Ваш комментарий..."
         ></textarea>
