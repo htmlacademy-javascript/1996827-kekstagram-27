a87b5f989193dad3d36eb4a4bfe555f9e37edd7e
kos <ustyugovki@bk.ru>
2022-11-04 21:33:22 +0300

    Сделал кнопки масштаба в окне редактирования изображения

--- js/main.js
+++ js/main.js
@@ -32,3 +32,48 @@ addEventListener('change', traceEvent, {capture: true});
 addEventListener('open', traceEvent, {capture: true});
 addEventListener('close', traceEvent, {capture: true});
 addEventListener('click', traceEvent, {capture: true});
+
+const {upload} = gallery;
+upload.dialog.display(true);
+
+// SCALE IMAGE
+const scaleControlValue = document.querySelector('.scale__control--value');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const imgUploadPreview = document.querySelector('.img-upload__preview');
+
+
+const SCALE_MIN = 25;
+const SCALE_MAX = 100;
+const SCALE_STEP = 25;
+
+scaleControlValue.value = 100;
+let scale = Number(scaleControlValue.value);
+scale = 100;
+
+
+const scalePreview = () => {
+  imgUploadPreview.style.transform = `scale(${scale / 100})`;
+};
+
+
+function scaleControlDown() {
+  if (scale > SCALE_MIN) {
+    scale -= SCALE_STEP;
+    scaleControlValue.value = scale;
+  }
+  scalePreview();
+}
+
+function scaleControlUp() {
+  if (scale < SCALE_MAX) {
+    scale += SCALE_STEP;
+    scaleControlValue.value = scale;
+  }
+  scalePreview();
+}
+
+scaleControlSmaller.addEventListener('click', scaleControlDown);
+scaleControlBigger.addEventListener('click', scaleControlUp);
+
+
