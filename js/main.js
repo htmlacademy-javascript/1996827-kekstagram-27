d54c78fd044aa48af1e856966912b1a79ea75cc8
kos <ustyugovki@bk.ru>
2022-11-05 21:38:55 +0300

    Исправил ошибки module9-task1

--- js/main.js
+++ js/main.js
@@ -40,47 +40,43 @@ upload.dialog.display(true);
 const scaleControlValue = document.querySelector('.scale__control--value');
 const scaleControlSmaller = document.querySelector('.scale__control--smaller');
 const scaleControlBigger = document.querySelector('.scale__control--bigger');
-const imgUploadPreview = document.querySelector('.img-upload__preview');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
 
 
 const SCALE_MIN = 25;
 const SCALE_MAX = 100;
 const SCALE_STEP = 25;
 
-scaleControlValue.value = 100;
-let scale = Number(scaleControlValue.value);
-scale = 100;
-
+let scale = 100;
+scaleControlValue.value = `${scale}%`;
 
 const scalePreview = () => {
+  scaleControlValue.value = `${scale}%`;
   imgUploadPreview.style.transform = `scale(${scale / 100})`;
 };
 
 
-function scaleControlDown() {
+function scaleControlDownHandler() {
   if (scale > SCALE_MIN) {
     scale -= SCALE_STEP;
-    scaleControlValue.value = scale;
   }
   scalePreview();
 }
 
-function scaleControlUp() {
+function scaleControlUpHandler() {
   if (scale < SCALE_MAX) {
     scale += SCALE_STEP;
-    scaleControlValue.value = scale;
   }
   scalePreview();
 }
 
-scaleControlSmaller.addEventListener('click', scaleControlDown);
-scaleControlBigger.addEventListener('click', scaleControlUp);
+scaleControlSmaller.addEventListener('click', scaleControlDownHandler);
+scaleControlBigger.addEventListener('click', scaleControlUpHandler);
 
 
 // FILTERS
 
 const effectsList = document.querySelector('.effects__list');
-const imgPreviewEffect = document.querySelector('.img-upload__preview').querySelector('img');
 const sliderContainer = document.querySelector('.img-upload__effect-level');
 const slider = sliderContainer.querySelector('.effect-level__slider');
 const effectLevel = sliderContainer.querySelector('.effect-level__value');
@@ -90,12 +86,22 @@ let currentEffect;
 sliderContainer.classList.add('hidden');
 
 const EffectChange = (event) => {
-  imgPreviewEffect.classList.remove(`effects__preview--${currentEffect}`);
+  scale = 100;
+  scaleControlValue.value = `${scale}%`;
+  imgUploadPreview.style.transform = `scale(${scale / 100})`;
+
+  imgUploadPreview.classList.remove(`effects__preview--${currentEffect}`);
   currentEffect = event.target.value;
-  imgPreviewEffect.classList.add(`effects__preview--${currentEffect}`);
+  imgUploadPreview.classList.add(`effects__preview--${currentEffect}`);
 
   if (currentEffect === 'none') {
     sliderContainer.classList.add('hidden');
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1,
+      },
+    });
   }
 
   if (currentEffect === 'chrome' || currentEffect === 'sepia') {
@@ -162,26 +168,26 @@ slider.noUiSlider.on('update', () => {
   effectLevel.value = slider.noUiSlider.get();
 
   if (currentEffect === 'none') {
-    imgPreviewEffect.style.filter = 'none';
+    imgUploadPreview.style.filter = 'none';
   }
 
   if (currentEffect === 'chrome') {
-    imgPreviewEffect.style.filter = `grayscale(${effectLevel.value})`;
+    imgUploadPreview.style.filter = `grayscale(${effectLevel.value})`;
   }
 
   if (currentEffect === 'sepia') {
-    imgPreviewEffect.style.filter = `sepia(${effectLevel.value})`;
+    imgUploadPreview.style.filter = `sepia(${effectLevel.value})`;
   }
 
   if (currentEffect === 'marvin') {
-    imgPreviewEffect.style.filter = `invert(${effectLevel.value}%)`;
+    imgUploadPreview.style.filter = `invert(${effectLevel.value}%)`;
   }
 
   if (currentEffect === 'phobos') {
-    imgPreviewEffect.style.filter = `blur(${effectLevel.value}px)`;
+    imgUploadPreview.style.filter = `blur(${effectLevel.value}px)`;
   }
   if (currentEffect === 'heat') {
-    imgPreviewEffect.style.filter = `brightness(${effectLevel.value})`;
+    imgUploadPreview.style.filter = `brightness(${effectLevel.value})`;
   }
 
 });
